1948ce4c9e4e24b0718a223bbaf115bb1507131b
Henrik Kniberg <h@kniberg.com>
2017-07-17 12:15:30 +0200

    GUI improvements

--- client/signIt.js
+++ client/signIt.js
@@ -1,100 +1,112 @@
-const editingVar = new ReactiveVar(false)
+const showSignatureFormVar = new ReactiveVar(false)
+const showEmailFormVar = new ReactiveVar(false)
 const joinMailingListVar = new ReactiveVar(true)
 const nameMissingVar = new ReactiveVar(false)
 const emailMissingVar = new ReactiveVar(false)
 const somethingWentWrongVar = new ReactiveVar(false)
-const signatureReceivedVar = new ReactiveVar(false)
+const signatureIdVar = new ReactiveVar(null)
 
 Template.signIt.helpers({
-  editing() {
-    return editingVar.get()
+  showSignatureForm() {
+    return showSignatureFormVar.get()
   },
-  "joinMailingList"() {
+  showEmailForm() {
+    return showEmailFormVar.get()
+  },
+  joinMailingList() {
     return joinMailingListVar.get()
   },
-  "nameMissing"() {
+  nameMissing() {
     return nameMissingVar.get()
   },
-  "emailMissing"() {
+  emailMissing() {
     return emailMissingVar.get()
   },
-  "somethingWentWrong"() {
+  somethingWentWrong() {
     return somethingWentWrongVar.get()
   },
-  "signatureReceived"() {
-    return signatureReceivedVar.get()
+  showSignItButton() {
+    return !showSignatureFormVar.get() && !showEmailFormVar.get() && !signatureIdVar.get()
+  },
+  hasSigned() {
+    return signatureIdVar.get()
   }
-
 })
 
 
 Template.signIt.events({
   "click .showForm"() {
-    editingVar.set(true)
+    showSignatureFormVar.set(true)
+  },
+  
+  "click .signButton"() {
+    submitSignatureForm()
   },
 
-  "change #joinMailingList"() {
-    joinMailingListVar.set(isJoinMailingListChecked())
-    nameMissingVar.set(false)
-    emailMissingVar.set(false)
+  "click .joinMailingListButton"() {
+    joinMailingList()
   },
-  
-  "click .submitForm"() {
-    submitForm()
-  },
-  
-  "click .cancelForm"() {
-    nameMissingVar.set(false)
-    emailMissingVar.set(false)
-    editingVar.set(false)
+
+  "click .cancelButton"() {
+    reset()
   }
 })
 
-function submitForm() {
+function submitSignatureForm() {
   nameMissingVar.set(false)
   emailMissingVar.set(false)
   somethingWentWrongVar.set(false)
-  signatureReceivedVar.set(false)
 
   const name = getFormValue("name")
   if (!name) {
     nameMissingVar.set(true)
     return
   }
-
-  let email
-  if (isJoinMailingListChecked()) {
-    email = getFormValue("email")
-    if (!email) {
-      emailMissingVar.set(true)
-      return
-    }
-  }
-
-
+  
   const signature = {
     name: name,
     languageCode: "en",
     country: getFormValue("country"),
-    mailingList: isJoinMailingListChecked(),
     info: getFormValue("info"),
-    comments: getFormValue("comments"),
-    email: email
+    comments: getFormValue("comments")
   }
 
-  Meteor.call("sign", signature, function(err) {
+  Meteor.call("sign", signature, function(err, signatureId) {
     if (err) {
       console.log("error while submitting:", err)
       somethingWentWrongVar.set(true)
     } else {
-      editingVar.set(false)
-      signatureReceivedVar.set(true)
+      console.log("Setting signatureIdVar", signatureId)
+      signatureIdVar.set(signatureId)
+      showSignatureFormVar.set(false)
+      showEmailFormVar.set(true)
     }
   })
 }
 
-function isJoinMailingListChecked() {
-  return $("#joinMailingList").prop("checked")
+function joinMailingList() {
+  const email = getFormValue("email")
+  if (!email) {
+    emailMissingVar.set(true)
+    return
+  }
+  Meteor.call("setEmail", signatureIdVar.get(), email, function(err) {
+    if (err) {
+      console.log("error while submitting:", err)
+      somethingWentWrongVar.set(true)
+    } else {
+      reset()
+    }     
+  })
+}
+
+function reset() {
+  showSignatureFormVar.set(false)
+  showEmailFormVar.set(false)
+
+  nameMissingVar.set(false)
+  emailMissingVar.set(false)
+  somethingWentWrongVar.set(null)
 }
 
 function getFormValue(name) {
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-rm -rf ../output/carbonmanifesto.tar.gz
+rm -rf ../output/carbon*.tar.gz
 meteor build ../output --architecture os.linux.x86_64
 cd ../output
-echo "in nodechef, type deploy -i carbonmanifesto"
+mv carbonmanifesto.tar.gz carbonvow.tar.gz
+echo "in nodechef, type deploy -i carbonvow"
 nodechef
