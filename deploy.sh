dc81df60078ea62a07116abbf507b343274f847d
Henrik Kniberg <h@kniberg.com>
2017-07-17 10:44:01 +0200

    carbonvow => carbonmanifesto And added some missing files.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-rm -rf ../output/carbonvow.tar.gz
+rm -rf ../output/carbonmanifesto.tar.gz
 meteor build ../output --architecture os.linux.x86_64
 cd ../output
-echo "in nodechef, type deploy -i carbonvow"
+echo "in nodechef, type deploy -i carbonmanifesto"
 nodechef
